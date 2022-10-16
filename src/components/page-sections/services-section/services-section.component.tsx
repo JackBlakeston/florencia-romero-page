3422799dea62b51c385a5a6f3f2a57a4720a600e
Jack Blakeston <jackblakeston93@gmail.com>
2022-10-16 14:08:23 +0200

    Finish services section

--- src/components/page-sections/services-section/services-section.component.tsx
+++ src/components/page-sections/services-section/services-section.component.tsx
@@ -1,19 +1,28 @@
 import React, { forwardRef } from 'react';
 
+import { useIntl } from 'react-intl';
+
 import classes from './services-section.module.scss';
 
-import { SERVICES } from '../../../constants/enums';
 import ServiceInfo from './service-info/service-info.component';
 
+import { SERVICES } from '../../../constants/enums';
+
 const ServicesSection = forwardRef<HTMLDivElement, unknown>((_, ref) => {
+  const intl = useIntl();
+
   const renderServices = () => {
     const serviceNames = Object.values(SERVICES);
     return serviceNames.map((service, index) => <ServiceInfo service={service} key={index} />);
   };
 
+  const title = intl.formatMessage({
+    id: 'app.sections.services.sectionTitle',
+  });
+
   return (
     <div ref={ref} className={classes.mainContainer}>
-      <div>SERVICIOS</div>
+      <div className={classes.sectionTitle}>{title}</div>
       <div className={classes.servicesContainer}>{renderServices()}</div>
     </div>
   );
