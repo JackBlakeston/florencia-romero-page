51219dee33cf3037b08549afcb50f3552416c379
Jack Blakeston <jackblakeston93@gmail.com>
2022-10-16 16:31:00 +0200

    Implement portfolio section

--- src/components/page-sections/portfolio-section/portfolio-section.component.tsx
+++ src/components/page-sections/portfolio-section/portfolio-section.component.tsx
@@ -1,7 +1,75 @@
-import React, { forwardRef } from "react";
+import React, { forwardRef } from 'react';
+
+import { useIntl } from 'react-intl';
+import { Carousel } from 'react-responsive-carousel';
+
+import classes from './portfolio-section.module.scss';
+import './carousel-styles.scss';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
+import { ATRAE_URL } from '../../../constants/strings';
+import AtraeLogo from '../../../assets/images/atrae-logo.png';
+
+const importAllImagesFromFolder = (r: __WebpackModuleApi.RequireContext) => {
+  return r.keys().map(r);
+};
+
+const getCarrouselItemName = (imageUrl: string) => {
+  const splitUrl = imageUrl.split('/');
+  const fileName = splitUrl[splitUrl.length - 1];
+  const itemName = fileName.split('.')[0];
+  const itemNameNoPrefix = itemName.split('-')[1];
+  return itemNameNoPrefix;
+};
 
 const PortfolioSection = forwardRef<HTMLDivElement, unknown>((_, ref) => {
-  return (<div ref={ref}></div>)
-})
+  const intl = useIntl();
+
+  const renderPortfolioCarrousel = () => {
+    const allImagesUrls = importAllImagesFromFolder(
+      require.context('../../../assets/images/portfolio-items', false, /\.(png|jpe?g|svg)$/),
+    ) as string[];
+    return (
+      <Carousel
+        showThumbs={false}
+        showIndicators={false}
+        showStatus={false}
+        infiniteLoop
+        centerMode
+        centerSlidePercentage={25}
+      >
+        {allImagesUrls.map((imageUrl, index) => {
+          const itemName = getCarrouselItemName(imageUrl);
+          return (
+            <div className={classes.carrouselItemContainer} key={index}>
+              <img className={classes.carrouselItemImage} src={imageUrl} alt={itemName} />
+            </div>
+          );
+        })}
+      </Carousel>
+    );
+  };
+
+  const title = intl.formatMessage({
+    id: 'app.sections.portfolio.sectionTitle',
+  });
+
+  const portfolioLinkText = intl.formatMessage({
+    id: 'app.sections.portfolio.atraeLink',
+  });
+
+  return (
+    <div ref={ref} className={classes.mainContainer}>
+      <div className={classes.sectionTitle}>{title}</div>
+      <div className={classes.portfolioLinkContainer}>
+        <a href={ATRAE_URL} target='_blank' rel='noopener noreferrer'>
+          <img src={AtraeLogo} alt='atrae-logo' />
+        </a>
+        <span>{portfolioLinkText}</span>
+      </div>
+      <div className={classes.carrouselContainer}>{renderPortfolioCarrousel()}</div>
+    </div>
+  );
+});
 
 export default PortfolioSection;
--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,4 +1,5 @@
-export const PHONE_NUMBER = "+34 610 999 034";
-export const EMAIL = "fctraduce@gmail.com";
-export const NAME = "FLORENCIA ROMERO";
-export const TRANSLATOR_LANGUAGES = "EN, FR > ES";
+export const PHONE_NUMBER = '+34 610 999 034';
+export const EMAIL = 'fctraduce@gmail.com';
+export const NAME = 'FLORENCIA ROMERO';
+export const TRANSLATOR_LANGUAGES = 'EN, FR > ES';
+export const ATRAE_URL = 'https://basededatos.atrae.org/user/florencia-cecilia-romero-gonzalo';
