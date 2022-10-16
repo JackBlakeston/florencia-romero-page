3732ab856cd42b3b6b4d8082eb7986a7a0e4c1f4
Jack Blakeston <jackblakeston93@gmail.com>
2022-10-16 22:31:19 +0200

    Add styles to about me section

--- src/components/page-sections/about-me-section/about-me-section.component.tsx
+++ src/components/page-sections/about-me-section/about-me-section.component.tsx
@@ -7,7 +7,7 @@ import classes from './about-me-section.module.scss';
 import Icon from '../../icon/icon.component';
 
 import portrait from '../../../assets/images/portrait.jpg';
-import atraeLogoBig from '../../../assets/images/atrae-logo-big.jpg';
+import atraeLogoBig from '../../../assets/images/atrae-logo-big.png';
 import { ATRAE_URL, LINKEDIN_URL, NAME, PROZ_URL, SKYPE_URL, TWITTER_URL } from '../../../constants/strings';
 
 const AboutMeSection = forwardRef<HTMLDivElement, unknown>((_, ref) => {
@@ -31,30 +31,28 @@ const AboutMeSection = forwardRef<HTMLDivElement, unknown>((_, ref) => {
       <div className={classes.sectionBodyContainer}>
         <div className={classes.linkSectionContainer}>
           <img src={portrait} alt='florencia-romero-portrait' className={classes.portrait} />
-          <span>{NAME}</span>
-          <span>{translatorLanguages}</span>
-          <div className={classes.linksSection}>
-            <div className={classes.linkRow}>
-              <a>
-                <Icon name='gmail' />
-              </a>
-              <a href={LINKEDIN_URL} target='_blank' rel='noopener noreferrer'>
-                <Icon name='linkedin' />
-              </a>
-              <a href={TWITTER_URL} target='_blank' rel='noopener noreferrer'>
-                <Icon name='twitter' />
-              </a>
-              <a href={PROZ_URL} target='_blank' rel='noopener noreferrer'>
-                <Icon name='proz' />
-              </a>
-              <a href={SKYPE_URL} target='_blank' rel='noopener noreferrer'>
-                <Icon name='skype' />
-              </a>
-            </div>
-            <a href={ATRAE_URL} target='_blank' rel='noopener noreferrer'>
-              <img src={atraeLogoBig} alt='atrae' className={classes.atraeLogo} />
+          <span className={classes.nameText}>{NAME}</span>
+          <span className={classes.languagesText}>{translatorLanguages}</span>
+          <div className={classes.linkRow}>
+            <a>
+              <Icon name='gmail' />
+            </a>
+            <a href={LINKEDIN_URL} target='_blank' rel='noopener noreferrer'>
+              <Icon name='linkedin' />
+            </a>
+            <a href={TWITTER_URL} target='_blank' rel='noopener noreferrer'>
+              <Icon name='twitter' />
+            </a>
+            <a href={PROZ_URL} target='_blank' rel='noopener noreferrer'>
+              <Icon name='proz' />
+            </a>
+            <a href={SKYPE_URL} target='_blank' rel='noopener noreferrer'>
+              <Icon name='skype' />
             </a>
           </div>
+          <a href={ATRAE_URL} target='_blank' rel='noopener noreferrer'>
+            <img src={atraeLogoBig} alt='atrae' className={classes.atraeLogo} />
+          </a>
         </div>
         <div className={classes.textSectionContainer}>{aboutMeText}</div>
       </div>
