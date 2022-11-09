69677443abdd4f8b1ca9a7d6672ee8fd2123c01a
Jack Blakeston <jackblakeston93@gmail.com>
2022-11-09 20:43:01 +0100

    Remove title sketch

--- src/components/page-sections/title-section/title-section.component.tsx
+++ src/components/page-sections/title-section/title-section.component.tsx
@@ -5,7 +5,6 @@ import { useIntl } from 'react-intl';
 import classes from './title-section.module.scss';
 
 import logo from '../../../assets/images/logo.png';
-import bookSketch from '../../../assets/images/book-sketch.png';
 
 import { NAME, TRANSLATOR_LANGUAGES } from '../../../constants/strings';
 
@@ -24,9 +23,7 @@ const TitleSection = forwardRef<HTMLDivElement, unknown>((_, ref) => {
         </span>
         <span>{TRANSLATOR_LANGUAGES}</span>
       </div>
-      <div className={classes.sketchesContainer}>
-        <img src={bookSketch} alt='book-sketch' className={classes.bookSketch} />
-      </div>
+      <div className={classes.subtitleContainer}></div>
     </div>
   );
 });
